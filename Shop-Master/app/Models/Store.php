b35eda3c9c0a609f47f8de56b20bf2910a76febe
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 00:22:09 +0330

    config store model

--- Shop-Master/app/Models/Store.php
+++ Shop-Master/app/Models/Store.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
