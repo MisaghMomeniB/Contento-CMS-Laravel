34d5c9b2ae8526c7f3d1b78cc739c8b8e4cc1510
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 09:51:58 +0330

    set auth controller

---
+++ Shop-Master/app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLogin() {
+        return view('auth.login');
+    }
+
+    Public Function showRegister() {
+        return view('');
+    }
+}
