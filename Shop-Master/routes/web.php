f8509449d0fa6a1bd1179660a7efee8da6f87f47
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 15:37:44 +0330

    define route for dashboard admin

--- Shop-Master/routes/web.php
+++ Shop-Master/routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Admin Dashboard
+Route::get('/admin/dashboard', [AuthController::class, 'adminDashboard'])
+->middleware('auth')
+->name('adminDashboard');
+
 // User Dashboard
 Route::get('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
 Route::post('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
