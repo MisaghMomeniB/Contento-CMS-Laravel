0f3cecda5712f5aa3f4d31a71a1a5e902c234a5f
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 09:53:40 +0330

    update and set tailwind cdn

--- Shop-Master/resources/views/auth/login.blade.php
+++ Shop-Master/resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=s, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ورود به حساب کاربری</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     
