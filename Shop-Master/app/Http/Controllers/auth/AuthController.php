85da219d827dfc4261af1219ac6f18915f183eb0
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 12:07:53 +0330

    define controller

--- Shop-Master/app/Http/Controllers/auth/AuthController.php
+++ Shop-Master/app/Http/Controllers/auth/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -13,5 +15,22 @@ class AuthController extends Controller
 
     Public Function showRegister() {
         return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|string|max:50',
+            'phone_number'=>'required|string|max:15|unique:users,phone_number',
+            'password'=>'required|string|min:6',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'phone_number'=> $request->phone_number,
+            'password'=>Hash::make($request->password),
+        ]);
+
+        return redirect()->route('showLogin')->with('success','ثبت نام با موفقیت انجام شد');
     }
 }
