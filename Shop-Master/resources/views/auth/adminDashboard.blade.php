14ef36dba22f3873116c7dc4dc85f0880b43666d
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-14 10:14:29 +0330

    add script js for slide

--- Shop-Master/resources/views/auth/adminDashboard.blade.php
+++ Shop-Master/resources/views/auth/adminDashboard.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>داشبورد مدیریت</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <link href="https://cdn.fontcdn.ir/Font/Persian/Vazir/Vazir.css" rel="stylesheet">
     <style>
         body {
