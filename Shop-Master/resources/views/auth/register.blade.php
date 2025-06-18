c616f21fd5a19b6d9678ac83447d280a58a00504
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-18 17:25:18 +0330

    update code front

--- Shop-Master/resources/views/auth/register.blade.php
+++ Shop-Master/resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
 
         <div>
           <label class="block text-gray-700 mb-1">نام کامل</label>
-          <input type="text" name="name" placeholder="مثلاً احسان مومنی" class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400">
+          <input type="text" name="name" placeholder="مثلاً میثاق مومنی" class="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400">
         </div>
 
         <div>
