feb1395b50bc48e3b45ae76c8f22db21494789a4
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-19 10:26:33 +0330

    change route

--- Shop-Master/routes/web.php
+++ Shop-Master/routes/web.php
@@ -10,28 +10,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['role:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return view('auth.adminDashboard');
-    })->name('adminDashboard');
-});
+// Route::middleware(['role:admin'])->group(function () {
+//     Route::get('/admin/dashboard', function () {
+//         return view('auth.adminDashboard');
+//     })->name('adminDashboard');
+// });
 
-Route::middleware(['role:owner'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard.owner');
-    })->name('dashboard');
-});
+// Route::middleware(['role:owner'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard.owner');
+//     })->name('dashboard');
+// });
 
 // // Admin Dashboard
-// Route::get('/admin/dashboard', [AuthController::class, 'adminDashboard'])
-// ->middleware('auth')
-// ->name('adminDashboard');
+Route::get('/admin/dashboard', [AuthController::class, 'adminDashboard'])
+->middleware('auth')
+->name('adminDashboard');
 
 Route::post('/admin/invoices/create', [AuthController::class, 'storeInvoice'])->name('createinvoice');
 
 // // User Dashboard
-// Route::get('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
-// Route::post('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
+Route::get('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
+Route::post('/dashboard', [AuthController::class,'dashboard'])->middleware('auth')->name('dashboard');
 
 // For Get and Post Login
 Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
