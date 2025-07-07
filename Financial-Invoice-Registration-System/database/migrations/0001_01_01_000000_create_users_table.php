8cfb98ec2d718d30f66502887855ba4ad8721a85
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-07 12:06:14 +0330

    create users migration

--- Financial-Invoice-Registration-System/database/migrations/0001_01_01_000000_create_users_table.php
+++ Financial-Invoice-Registration-System/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('mobile', 11)->unique();
+            $table->string('phone')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('national_id')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('registration_number')->nullable();
+            $table->string('economic_number')->nullable();
+            $table->string('address')->nullable();
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            // $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
