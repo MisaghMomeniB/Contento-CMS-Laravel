73ee45ed8c80309281ebe4d3b447b4844d72eeea
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-07 15:43:24 +0330

    create migration and model for payments

---
+++ Financial-Invoice-Registration-System/database/migrations/2025_07_07_121051_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained()->onDelete('cascade');
+            $table->string('payment_date');
+            $table->string('amount');
+            $table->string('method');
+            $table->string('reference_number');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
