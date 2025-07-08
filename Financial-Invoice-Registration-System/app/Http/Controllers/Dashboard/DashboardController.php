a0be59a111e97bd43b7d649da3a199afaebfc157
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-09 00:46:31 +0330

    edit update dash

--- Financial-Invoice-Registration-System/app/Http/Controllers/Dashboard/DashboardController.php
+++ Financial-Invoice-Registration-System/app/Http/Controllers/Dashboard/DashboardController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function show()
     {
         return view("dashboard.admin");
+    }
+
+    public function fetchUsers(Request $request)
+    {
+        $users = User::all();
+        return view("admin.partials.users-list", compact("users"))->render();
     }
 }
