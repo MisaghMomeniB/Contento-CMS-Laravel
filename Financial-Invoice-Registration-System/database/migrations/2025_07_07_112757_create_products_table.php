8e54e113d83ebc36b1c26e6340d563538324444b
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-08 09:29:40 +0330

    change and debug product migration

--- Financial-Invoice-Registration-System/database/migrations/2025_07_07_112757_create_products_table.php
+++ Financial-Invoice-Registration-System/database/migrations/2025_07_07_112757_create_products_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
-            $table->string('product_code');
+            $table->text('description');
+            $table->string('product_code')->unique();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->string('price');
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
