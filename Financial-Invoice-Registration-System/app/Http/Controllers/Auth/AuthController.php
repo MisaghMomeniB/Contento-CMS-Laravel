53134c4a4e8d4c41609ca5644fe73562e166ce10
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-08 13:15:18 +0330

    add and create auth check

--- Financial-Invoice-Registration-System/app/Http/Controllers/Auth/AuthController.php
+++ Financial-Invoice-Registration-System/app/Http/Controllers/Auth/AuthController.php
@@ -46,7 +46,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($validate)) {
             $request->session()->regenerate();
-            return redirect()->route('home');
+            return redirect()->route('dashboard');
         }
     }
 
