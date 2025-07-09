e2bb5ba7f24eb17d264a52ad840ac5077c5dfe0b
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-09 14:11:37 +0330

    define customer model for submit and create new customer

--- Financial-Invoice-Registration-System/app/Models/Customer.php
+++ Financial-Invoice-Registration-System/app/Models/Customer.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $table = "users";
+
+    protected $fillable = [
+            'first_name',
+            'last_name',
+            'mobile',
+            'phone',
+            'postal_code',
+            'national_id',
+            'company_name',
+            'registration_number',
+            'economic_number',
+            'address',
+            'password',
+    ];
 }
