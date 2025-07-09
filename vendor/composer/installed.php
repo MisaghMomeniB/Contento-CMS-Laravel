48bd19d752387cce34a392ad11005e7db88ecf98
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-09 16:22:40 +0330

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a167495701d88d0d452954834d1b503c630e6183',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a167495701d88d0d452954834d1b503c630e6183',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kavenegar/laravel' => array(
+            'pretty_version' => 'v1.3.8',
+            'version' => '1.3.8.0',
+            'reference' => 'beae4f10bf6502429f2622c9a934a86fb2b798a6',
+            'type' => 'laravel-package',
+            'install_path' => __DIR__ . '/../kavenegar/laravel',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kavenegar/php' => array(
+            'pretty_version' => 'v1.2.2',
+            'version' => '1.2.2.0',
+            'reference' => '884561b8d8c91b01f7db25284c2be9923c809412',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kavenegar/php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
