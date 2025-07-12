663091cc0601b5698226d78449e4001d80ba05c5
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-12 13:00:31 +0330

    update compact for product list

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,9 +12,9 @@ class AdminController extends Controller
     {
         $realCount = User::where('type', 'حقیقی')->count();
         $legalCount = User::where('type', 'حقوقی')->count();
-        // $productCount = Product::count();
+        $productCount = Product::count();
         // شما می‌تونید فاکتورها و پرداختی‌ها رو اینجا هم بگیرید اگر میخواید نمایش بدید
 
-        return view('dashboards.admin', compact('realCount', 'legalCount',));
+        return view('dashboards.admin', compact('realCount', 'legalCount', 'productCount'));
     }
 }
