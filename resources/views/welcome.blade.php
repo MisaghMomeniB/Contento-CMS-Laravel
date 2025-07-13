5139d2b75b8b626fd4b63f5e055061d418e0f62e
MisaghMomeniB <jenabemisagh@gmail.com>
2025-07-14 00:14:58 +0330

    change title name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>صفحه لندینگ مدرن</title>
+    <title>صفحه اصلی</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         /* انیمیشن برای فید شدن محتوا */
