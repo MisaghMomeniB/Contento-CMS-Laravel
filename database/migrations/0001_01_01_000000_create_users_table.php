1c3d863e8578ea356bcef44254dd3e09c916258c
MisaghMomeniB <jenabemisagh@gmail.com>
2025-09-25 20:55:53 +0330

    update unique method

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
