5bf7320a17229de9b1659b17da8a1f6dbde9e5a6
MisaghMomeniB <jenabemisagh@gmail.com>
2025-09-29 12:17:45 +0330

    pdo arrays

--- config/db_config.php
+++ config/db_config.php
@@ -8,3 +8,10 @@ define ("DB_PASS", "your_pass");
 
 // DSN
 $dsn = "mysql:host=" . DB_HOST > ";db_name=" . DB_NAME;
+
+// PDO Arrays
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
