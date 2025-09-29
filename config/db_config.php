26e62931f98b79952ea3d1f43816b97a9adfa7cd
MisaghMomeniB <jenabemisagh@gmail.com>
2025-09-29 12:14:20 +0330

    create variable for dsn / add config

--- config/db_config.php
+++ config/db_config.php
@@ -5,3 +5,6 @@ define ("DB_HOST", "localhost");
 define ("DB_NAME", "your_database");
 define ("DB_USER", "your_user");
 define ("DB_PASS", "your_pass");
+
+// DSN
+$dsn = "mysql:host=" . DB_HOST > ";db_name=" . DB_NAME;
