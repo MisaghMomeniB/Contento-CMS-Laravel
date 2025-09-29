e9f62752b33d06933c8d2acf01f5d8e927f3a789
MisaghMomeniB <jenabemisagh@gmail.com>
2025-09-29 12:21:00 +0330

    update / define variable for pdo connection

--- config/db_config.php
+++ config/db_config.php
@@ -13,5 +13,8 @@ $dsn = "mysql:host=" . DB_HOST > ";db_name=" . DB_NAME;
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
+
+// Connection Variable
+$pdo = null;
